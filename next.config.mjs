3bf821bda970edb41fabb0074ac3309371fd0eaf
firstproject <yadavkanchan0109@gmail.com>
2026-03-20 19:03:45 +0530

    ignore eslint errors

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,9 @@ const nextConfig = {
     images: {
     domains: ['www.narayansevausa.org', 'images.unsplash.com','images.unsplash.com', 'source.unsplash.com','cdn.pixabay.com','pixabay.com']
   },
-
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
